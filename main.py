95f3670ae9856cd5206daf5f33dfb05e9b431678
AtharvNavle <atharvnavle@gmail.com>
2026-04-21 00:32:04 +0530

    scheked validntn

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
+from pydantic import BaseModel 
+from pydantic import EmailStr, Field
 
 app = FastAPI()
 
+
+#defining da model firstly
+class User(BaseModel):
+    id : int
+    name : str
+    email : EmailStr | None = Field(default = None)
+
+users_db = []
+
 @app.get("/")
 async def root():
-    return {"message:hello"}
+    return {"message":"woosah"}
+
+@app.post("/woosah")
+async def create_user(user: User):
+    users_db.append(user)
+    return {
+        "id":user.id,
+        "name":user.name,
+        "email":user.email
+    }
+@app.get("/users")
+async def get_user():
+    return users_db
